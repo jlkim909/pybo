2fff3c5a3fd89c6a15859841e184e650e2b2214a
ymg <jlkim909@gmail.com>
2021-03-16 14:32:04 +0900

    Database 등록

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,13 @@ ALLOWED_HOSTS = ['3.36.198.59']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': ':2+XH,e&)J;v&^0NAdQPDPFn^xqW(o03',
+        'HOST': 'ls-6497a8e2c71bd8093be073a76a6be698b14186a7.chdk0lih9kbr.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
